e8b701aafc9fdd4ea1bdde364259701da3b68398
mohamed-faaris <mohammedfaaris2005@gmail.com>
2025-09-11 02:01:08 +0530

    working on backend 1

--- backend/config/db.py
+++ backend/config/db.py
@@ -1,17 +1,17 @@
 import os
 from dotenv import load_dotenv
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from pymongo import MongoClient
 
 load_dotenv()
+
 uri = os.getenv("MONGODB_URI")
-client = MongoClient(uri, server_api=ServerApi("1"))
+print(f"Connecting to MongoDB at {uri}")
+conn = MongoClient(uri)
 
 try:
-    client.admin.command("ping")
+    conn.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
     
-db = client.profileDB
-collection_name = db[]
+db = conn['myapp']
